aef46331ab22deff8b00a4391950d86c9e7fe22b
Felipe Rodrigues <felipersdf@hotmail.com>
2020-04-23 15:58:54 -0300

    set authorization

--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -2,8 +2,9 @@ import React, { useCallback, useRef }from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core'; 
-
 import * as Yup from 'yup';
+
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErros from '../../utils/getValidationErros';
 
 import logoImg from '../../assets/logo.svg';
@@ -13,21 +14,35 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+};
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { user, signIn } = useAuth();
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail obrigatório')
-          .email('Digite um email válido'),
-        password: Yup.string()
-          .required('Senha obrigatória'),
+  console.log(user);
+
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors([]);
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um email válido'),
+          password: Yup.string().required('Senha obrigatória'),
       });
       
       await schema.validate(data, {
         abortEarly: false,
+      });
+
+      signIn({
+        email: data.email,
+        password: data.password,
       });
     } catch (err) {
       console.log(err);
@@ -36,7 +51,8 @@ const SignIn: React.FC = () => {
 
       formRef.current?.setErrors(errors);
     }
-  }, []);
+  }, [signIn]);
+
   return (
     <Container>
       <Content>
