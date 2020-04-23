d585e8db9835302f9deb44c89ef25235626c0551
Felipe Rodrigues <felipersdf@hotmail.com>
2020-04-23 14:55:15 -0300

    finishing visual part

--- gobarber-web/src/components/Input/styles.ts
+++ gobarber-web/src/components/Input/styles.ts
@@ -1,6 +1,14 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   color: #666360;
   background: #232129;
   border-radius: 10px;
@@ -14,6 +22,19 @@ export const Container = styled.div`
   & + div {
     margin-top: 8px;
   }
+
+  ${props => props.isErrored && css`
+    border-color: #C53030;
+  `}
+ 
+  ${props => props.isFocused && css`
+    color: #FF9000;
+    border-color: #FF9000;
+  `}
+
+  ${props => props.isFilled && css`
+    color: #FF9000;
+  `}
 
   input {
     flex: 1;
@@ -30,3 +51,21 @@ export const Container = styled.div`
       margin-right: 16px;
     }
 `;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #C53030;
+    color: #FFF;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+`
--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,8 +1,7 @@
 import React, { useCallback, useRef }from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-import { Form } from '@unform/web';
-import { FormHandles } from '@unform/core'; 
-
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web'
 import * as Yup from 'yup';
 import getValidationErros from '../../utils/getValidationErros';
 
@@ -13,19 +12,22 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => {
+const SignUp: React.FC = () => {  
   const formRef = useRef<FormHandles>(null);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors([]);
+      
       const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um email válido'),
         password: Yup.string()
-          .required('Senha obrigatória'),
+          .min(6, 'No mínimo 6 dígitos'),
       });
-      
+
       await schema.validate(data, {
         abortEarly: false,
       });
@@ -39,32 +41,31 @@ const SignIn: React.FC = () => {
   }, []);
   return (
     <Container>
+      <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
-          
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input 
-            name="password"
-            icon={FiLock}
-            type="password" 
-            placeholder="Senha" 
-          />
-          
-          <Button type="submit">Entrar</Button>
-
-          <a href="forgot">Esqueci minha senha</a>
-        </Form>
-          <a href="login">
-            <FiLogIn />
-            Criar Conta
-          </a>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Faça seu cadastro</h1> 
+        
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input 
+          name="password"
+          icon={FiLock}
+          type="password" 
+          placeholder="Senha" 
+        />
+        
+        <Button type="submit">Cadastrar</Button>
+      </Form>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
       </Content>
-      <Background />
     </Container>
   )
 };
 
-export default SignIn;
+export default SignUp;
