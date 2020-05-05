9a1069e3dd78b6c761db4a55ce90803efcd5cab0
Felipe Rodrigues <felipersdf@hotmail.com>
2020-05-05 11:24:58 -0300

    add update user avatar test

--- iniciando-back-end/src/config/upload.ts
+++ iniciando-back-end/src/config/upload.ts
@@ -2,14 +2,15 @@ import path from 'path';
 import multer from 'multer';
 import crypto from 'crypto';
 
-const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp'),
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-   directory: tmpFolder,
+   tmpFolder,
+   uploadsFolder: path.resolve(tmpFolder, 'uplaods'),
 
    storage: multer.diskStorage({
       destination: tmpFolder,
-      filename (request, file, callback) {
+      filename(request, file, callback) {
          const fileHash = crypto.randomBytes(10).toString('HEX');
          const fileName = `${fileHash}-${file.originalname}`;
 
