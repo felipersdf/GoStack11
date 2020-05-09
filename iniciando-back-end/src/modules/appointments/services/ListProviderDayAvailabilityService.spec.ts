7c760f04c510e7f96322c4099cbff06b69aab42e
Felipe Rodrigues <felipersdf@hotmail.com>
2020-05-09 19:46:06 -0300

    list day availability for appointments

---
+++ iniciando-back-end/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -0,0 +1,45 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+import ListProviderDayAvailabilityService from './ListProviderDayAvailabilityService';
+
+let listProviderDayAvailability: ListProviderDayAvailabilityService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviderDayAvailability', () => {
+   beforeEach(() => {
+      listProviderDayAvailability = new ListProviderDayAvailabilityService(
+         fakeAppointmentsRepository,
+      );
+      fakeAppointmentsRepository = new FakeAppointmentsRepository();
+   });
+
+   it('should be able to list the month availability from provider', async () => {
+      await fakeAppointmentsRepository.create({
+         provider_id: 'user',
+         date: new Date(2020, 4, 20, 8, 0, 0),
+      });
+
+      await fakeAppointmentsRepository.create({
+         provider_id: 'user',
+         date: new Date(2020, 4, 20, 10, 0, 0),
+      });
+
+      const availability = await listProviderDayAvailability.execute({
+         provider_id: 'user',
+         day: 20,
+         month: 5,
+         year: 2020,
+      });
+
+      expect(availability).toEqual(
+         expect.arrayContaining([
+            { hour: 8, available: false },
+            { hour: 9, available: true },
+            { hour: 10, available: false },
+            { hour: 11, available: true },
+         ]),
+      );
+   });
+});
